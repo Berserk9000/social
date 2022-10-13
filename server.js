b08f8b1e90feee19f8ead19536de41334d9f2806
Berserk9000 <cruz.d.kevin@gmail.com>
2022-10-12 20:51:52 -0400

    working on user routes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 app.use(require('./routes'));
 
 // Connect mongoose
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social', {
   useFindAndModify: false,
   useUnifiedTopology: true
 });
